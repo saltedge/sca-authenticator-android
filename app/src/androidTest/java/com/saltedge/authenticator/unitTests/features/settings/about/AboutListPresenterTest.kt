c2ce0630bad14a82b01999ea9c9063ceccf2e1e5
constantink <constantink@saltedge.com>
2019-07-23 14:28:13 +0000

    2.0.0-rc5

--- app/src/androidTest/java/com/saltedge/authenticator/unitTests/features/settings/about/AboutListPresenterTest.kt
+++ app/src/androidTest/java/com/saltedge/authenticator/unitTests/features/settings/about/AboutListPresenterTest.kt
@@ -52,7 +52,7 @@ class AboutListPresenterTest {
         assertThat(presenter.getListItems(), equalTo(listOf(
                 CheckedTitleValueViewModel(
                         titleId = R.string.about_app_version,
-                        value = "2.0.0-rc4"
+                        value = "2.0.0-rc5"
                 ),
                 CheckedTitleValueViewModel(
                         titleId = R.string.about_copyright,
