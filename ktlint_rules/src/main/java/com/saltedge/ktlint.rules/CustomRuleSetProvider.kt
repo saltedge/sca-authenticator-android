9da7edfd4f08e1ce2ca5171389cb5e3373e09798
vadimm <vadimm@saltedge.com>
2019-08-07 13:00:50 +0300

    Added ktlint plugin to project as module, also added CommentSpacingRule

---
+++ ktlint_rules/src/main/java/com/saltedge/ktlint.rules/CustomRuleSetProvider.kt
@@ -0,0 +1,12 @@
+package com.saltedge.ktlint.rules
+
+import com.pinterest.ktlint.core.RuleSet
+import com.pinterest.ktlint.core.RuleSetProvider
+
+class CustomRuleSetProvider : RuleSetProvider {
+
+    override fun get(): RuleSet = RuleSet(
+            "custom",
+            CommentSpacingRule()
+    )
+}
