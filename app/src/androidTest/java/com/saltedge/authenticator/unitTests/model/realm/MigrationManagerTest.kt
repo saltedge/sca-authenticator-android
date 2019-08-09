c356c645b42d46ff34e3d0b1ed846616c9dba1f7
mnewlive <morozovvadim91@gmail.com>
2019-08-09 18:30:47 +0300

    Added test on migrations

---
+++ app/src/androidTest/java/com/saltedge/authenticator/unitTests/model/realm/MigrationManagerTest.kt
@@ -0,0 +1,86 @@
+package com.saltedge.authenticator.unitTests.model.realm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.saltedge.authenticator.model.db.Connection
+import com.saltedge.authenticator.model.realm.DB_SCHEMA_VERSION
+import com.saltedge.authenticator.model.realm.runMigrations
+import com.saltedge.authenticator.testTools.TestTools
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+
+private const val INITIAL_DB_NAME = "initial.realm"
+private const val INITIAL_DB_PATH = "db/$INITIAL_DB_NAME"
+private const val INITIAL_DB_SIZE = 8192
+
+@RunWith(AndroidJUnit4::class)
+class MigrationManagerTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun testMigration() {
+        Realm.init(TestTools.applicationContext)
+        val config = RealmConfiguration.Builder()
+            .name(INITIAL_DB_NAME)
+            .schemaVersion(DB_SCHEMA_VERSION)
+            .migration(runMigrations())
+            .build()
+
+        assertThat(copyInitialRealmFile(INITIAL_DB_PATH, config.path), equalTo(INITIAL_DB_SIZE))
+        assertThat(File(config.path).length(), equalTo(INITIAL_DB_SIZE.toLong()))
+
+        Realm.getInstance(config).use {
+            testMigration2(it)
+        }
+    }
+
+    private fun testMigration2(realm: Realm) {
+        val connection = realm.where(Connection::class.java).findFirst()
+
+        assertNull(connection?.supportEmail)
+    }
+
+    private fun copyInitialRealmFile(sourcePath: String, targetPath: String): Int {
+        removeTargetFile(targetPath)
+        return copyFile(sourcePath, targetPath)
+    }
+
+    private fun copyFile(sourcePath: String, targetPath: String): Int {
+        var inputStream: InputStream? = null
+        var outputStream: FileOutputStream? = null
+        var totalBytesRead = 0
+        try {
+            val file = File(targetPath)
+            inputStream =
+                InstrumentationRegistry.getInstrumentation().context.resources.assets.open(
+                    sourcePath
+                )
+            outputStream = FileOutputStream(file)
+            val buffer = ByteArray(1024)
+            var bytesRead = inputStream.read(buffer)
+            while (bytesRead > 0) {
+                totalBytesRead += bytesRead
+                outputStream.write(buffer, 0, bytesRead)
+                bytesRead = inputStream.read(buffer)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            inputStream?.close()
+            outputStream?.close()
+        }
+        return totalBytesRead
+    }
+
+    private fun removeTargetFile(targetPath: String) {
+        with(File(targetPath)) { if (exists()) delete() }
+    }
+}
