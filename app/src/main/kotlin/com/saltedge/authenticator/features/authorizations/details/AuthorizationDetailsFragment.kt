8c8153006e09dcde5d9aa09e3ca5426960f6ee80
Constantin Chelban <android.fedora.md@gmail.com>
2019-09-06 13:45:33 +0300

    removed authorizations top menu, fixed authorization details error handling

--- app/src/main/kotlin/com/saltedge/authenticator/features/authorizations/details/AuthorizationDetailsFragment.kt
+++ app/src/main/kotlin/com/saltedge/authenticator/features/authorizations/details/AuthorizationDetailsFragment.kt
@@ -27,6 +27,7 @@ import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import com.saltedge.authenticator.R
 import com.saltedge.authenticator.app.KEY_MODE
 import com.saltedge.authenticator.app.TIME_VIEW_UPDATE_TIMEOUT
@@ -45,8 +46,10 @@ import kotlinx.android.synthetic.main.fragment_authorization_details.*
 import java.util.*
 import javax.inject.Inject
 
-class AuthorizationDetailsFragment : BaseFragment(), AuthorizationDetailsContract.View,
-    View.OnClickListener {
+class AuthorizationDetailsFragment : BaseFragment(),
+    AuthorizationDetailsContract.View,
+    View.OnClickListener
+{
 
     @Inject
     lateinit var presenterContract: AuthorizationDetailsPresenter
@@ -157,8 +160,21 @@ class AuthorizationDetailsFragment : BaseFragment(), AuthorizationDetailsContrac
         actionsLayout?.isEnabled = show
     }
 
+    override fun showError(errorMessage: String) {
+        view?.let { Snackbar.make(it, errorMessage, Snackbar.LENGTH_LONG).show() }
+    }
+
     override fun closeView() {
         activity?.finishFragment()
+    }
+
+    override fun closeViewWithErrorResult(errorMessage: String) {
+        completeView?.setTitleText(getString(R.string.authorizations_finished_error))
+        completeView?.setSubtitleText(errorMessage)
+        completeView?.setIconResource(R.drawable.ic_complete_error_70)
+        completeView?.alpha = 0.75f
+        completeView?.setVisible(true)
+        completeView?.animate()?.alpha(1f)?.setDuration(1000)?.withEndAction { closeView() }
     }
 
     override fun closeViewWithSuccessResult(authorizationId: String) {
