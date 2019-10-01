53047b1576bc1fd9b502a27d39e58c11e761ba43
mnewlive <morozovvadim91@gmail.com>
2019-10-01 16:07:05 +0300

    Added license

--- app/src/main/kotlin/com/saltedge/authenticator/features/connections/common/RoundImageView.kt
+++ app/src/main/kotlin/com/saltedge/authenticator/features/connections/common/RoundImageView.kt
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the Salt Edge Authenticator distribution
+ * (https://github.com/saltedge/sca-authenticator-android).
+ * Copyright (c) 2019 Salt Edge Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 or later.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For the additional permissions granted for Salt Edge Authenticator
+ * under Section 7 of the GNU General Public License see THIRD_PARTY_NOTICES.md
+ */
 package com.saltedge.authenticator.features.connections.common
 
 import android.content.Context
@@ -38,8 +58,8 @@ class RoundedImageView : AppCompatImageView {
     private fun Bitmap.toCircleBitmap(diameter: Int): Bitmap =
         emptyCircle(diameter).also {
             bgPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-            val left = (it.width - this.width) /2
-            val top = (it.height - this.height) /2
+            val left = (it.width - this.width) / 2
+            val top = (it.height - this.height) / 2
             Canvas(it).drawBitmap(this, left.toFloat(), top.toFloat(), bgPaint)
             bgPaint.xfermode = null
         }
