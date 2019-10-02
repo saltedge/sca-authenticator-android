ef66039b1cf5e898735fc994fc85a498ce05640e
mnewlive <morozovvadim91@gmail.com>
2019-10-02 18:07:58 +0300

    Added string res

--- app/src/main/kotlin/com/saltedge/authenticator/features/connections/connect/ConnectProviderPresenter.kt
+++ app/src/main/kotlin/com/saltedge/authenticator/features/connections/connect/ConnectProviderPresenter.kt
@@ -114,12 +114,12 @@ class ConnectProviderPresenter @Inject constructor(
         } else if (result != null && result.isValid()) {
             val providerData = Connection().initWithProviderData(result)
             if (providerData == null) {
-                viewContract?.showErrorAndFinish("Something went wrong. Unable connect to Provider")
+                viewContract?.showErrorAndFinish(appContext.getString(R.string.errors_provider))
             } else {
                 this.connection = providerData
                 performNewConnectionRequest()
             }
-        } else viewContract?.showErrorAndFinish("Something went wrong. Unable connect to Provider")
+        } else viewContract?.showErrorAndFinish(appContext.getString(R.string.errors_provider))
     }
 
     override fun onConnectionInitSuccess(response: AuthenticateConnectionData) {
