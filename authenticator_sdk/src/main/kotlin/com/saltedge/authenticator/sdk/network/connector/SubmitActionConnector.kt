318c24b8a59fc8ae8d341d027955b914fa32255b
mnewlive <morozovvadim91@gmail.com>
2020-06-02 17:16:43 +0300

    Rm unused code

--- authenticator_sdk/src/main/kotlin/com/saltedge/authenticator/sdk/network/connector/SubmitActionConnector.kt
+++ authenticator_sdk/src/main/kotlin/com/saltedge/authenticator/sdk/network/connector/SubmitActionConnector.kt
@@ -20,7 +20,6 @@
  */
 package com.saltedge.authenticator.sdk.network.connector
 
-import android.util.Log
 import com.saltedge.authenticator.sdk.constants.API_ACTIONS
 import com.saltedge.authenticator.sdk.constants.REQUEST_METHOD_PUT
 import com.saltedge.authenticator.sdk.contract.ActionSubmitListener
@@ -48,7 +47,6 @@ internal class SubmitActionConnector(
             accessToken = connectionAndKey.connection.accessToken,
             signPrivateKey = connectionAndKey.key
         )
-        Log.d("some", "requestData : $requestData")
 
         apiInterface.updateAction(
             requestData.requestUrl,
@@ -59,7 +57,6 @@ internal class SubmitActionConnector(
 
     override fun onSuccessResponse(call: Call<SubmitActionResponse>, response: SubmitActionResponse) {
         val data = response.data
-        Log.d("some", "onSuccessResponse data : $data")
         if (data == null) {
             onFailureResponse(call, createInvalidResponseError())
         } else {
