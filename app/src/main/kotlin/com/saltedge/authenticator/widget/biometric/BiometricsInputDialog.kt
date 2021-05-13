99987a5d0b6b4dcb8fc220b294bfb69e84fb4d17
Constantin Chelban <android.fedora.md@gmail.com>
2021-05-13 14:51:17 +0300

    remove unused imports

--- app/src/main/kotlin/com/saltedge/authenticator/widget/biometric/BiometricsInputDialog.kt
+++ app/src/main/kotlin/com/saltedge/authenticator/widget/biometric/BiometricsInputDialog.kt
@@ -21,7 +21,6 @@
 package com.saltedge.authenticator.widget.biometric
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.animation.CycleInterpolator
 import android.widget.ImageView
