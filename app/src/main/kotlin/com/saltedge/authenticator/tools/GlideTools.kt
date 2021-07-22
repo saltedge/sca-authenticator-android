cae0a56bbfee7d1f01433b2671e01fb12ce1cd53
mnewlive <morozovvadim91@gmail.com>
2021-07-22 16:20:24 +0300

    Fixed icons in Connections/Authorizations/Consents lists

--- app/src/main/kotlin/com/saltedge/authenticator/tools/GlideTools.kt
+++ app/src/main/kotlin/com/saltedge/authenticator/tools/GlideTools.kt
@@ -1,12 +1,9 @@
 package com.saltedge.authenticator.tools
 
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.saltedge.authenticator.R
-import com.saltedge.authenticator.widget.RoundedBitmapTransformation
+import com.google.android.material.imageview.ShapeableImageView
 import timber.log.Timber
 
 private val imageLoaderOptions = RequestOptions()
@@ -19,7 +16,7 @@ private val imageLoaderOptions = RequestOptions()
  * @param imageUrl - remote image url
  * @param placeholderId - image to be set as default
  */
-fun ImageView.loadImage(imageUrl: String?, placeholderId: ResId, cornerRadius: Float = -1f) {
+fun ShapeableImageView.loadImage(imageUrl: String?, placeholderId: ResId) {
     try {
         Glide.with(context)
             .load(imageUrl)
