a6cd06b3537fe4cb1e1f93f6e5b36828b0c1df68
mnewlive <morozovvadim91@gmail.com>
2021-08-20 13:03:53 +0300

    Modified tests

--- app/src/test/java/com/saltedge/authenticator/features/authorizations/list/AuthorizationsListInteractorV2Test.kt
+++ app/src/test/java/com/saltedge/authenticator/features/authorizations/list/AuthorizationsListInteractorV2Test.kt
@@ -88,8 +88,8 @@ class AuthorizationsListInteractorV2Test : CoroutineViewModelTest() {
     private val authorizations: List<AuthorizationV2Data> = listOf(
         createAuthorization(id = 1),
         createAuthorization(id = 2),
-        createAuthorization(id = 3, status = "closed")
-//        ,createAuthorization(id = 4, status = "confirmed", finishedAt = DateTime.now()) TODO: Add authorization with final status to authorizations list
+        createAuthorization(id = 3, status = "closed"),
+        createAuthorization(id = 4, status = "confirmed", finishedAt = DateTime.now())
     )
     private val encryptedAuthorizations: List<AuthorizationResponseData> = authorizations.map { it.encryptWithTestKey() }
     private val items: List<AuthorizationItemViewModel> = authorizations
@@ -206,12 +206,97 @@ class AuthorizationsListInteractorV2Test : CoroutineViewModelTest() {
     fun onFetchAuthorizationsResultTestCase4() {
         //given
         val fetchResult = encryptedAuthorizations
+        val createdAt = DateTime.now(DateTimeZone.UTC)
 
         //when
         interactor.onFetchAuthorizationsResult(result = fetchResult, errors = emptyList())
 
         //then
-        verify(mockContract).onAuthorizationsReceived(data = items, newModelsApiVersion = API_V2_VERSION)
+        verify(mockContract).onAuthorizationsReceived(
+            data = listOf(
+                AuthorizationItemViewModel(
+                    authorizationID = "1",
+                    authorizationCode = "111",
+                    title = "title1",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc1",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "2",
+                    authorizationCode = "222",
+                    title = "title2",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc2",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "3",
+                    authorizationCode = "333",
+                    title = "title3",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc3",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "4",
+                    authorizationCode = "",
+                    title = "",
+                    description = DescriptionData(
+                        payment = null,
+                        text = null,
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 0,
+                    endTime = DateTime(0),
+                    startTime = DateTime(0),
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.CONFIRMED,
+                    geolocationRequired = true
+                )
+            ),
+            newModelsApiVersion = API_V2_VERSION
+        )
     }
 
     @Test
@@ -232,6 +317,7 @@ class AuthorizationsListInteractorV2Test : CoroutineViewModelTest() {
         val finalResponseModelWithExpiredFinishedAt = finalResponseModel
             .copy(id = "333", finishedAt = DateTime.now().minusSeconds(LIFE_TIME_OF_FINAL_MODEL + 100))
         val fetchResult = encryptedAuthorizations + listOf(finalResponseModel, finalResponseModelWithOutFinishedAt, finalResponseModelWithExpiredFinishedAt)
+        val createdAt = DateTime.now(DateTimeZone.UTC)
 
         //when
         interactor.onFetchAuthorizationsResult(result = fetchResult, errors = emptyList())
@@ -254,8 +340,110 @@ class AuthorizationsListInteractorV2Test : CoroutineViewModelTest() {
         ).apply {
             updateDestroyAt(finalResponseModel.finishedAt)
         }
-        val expectedResult = items + finalItem
-        verify(mockContract).onAuthorizationsReceived(data = expectedResult, newModelsApiVersion = API_V2_VERSION)
+        verify(mockContract).onAuthorizationsReceived(
+            data = listOf(
+                AuthorizationItemViewModel(
+                    authorizationID = "1",
+                    authorizationCode = "111",
+                    title = "title1",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc1",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "2",
+                    authorizationCode = "222",
+                    title = "title2",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc2",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "3",
+                    authorizationCode = "333",
+                    title = "title3",
+                    description = DescriptionData(
+                        payment = null,
+                        text = "desc3",
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 180,
+                    endTime = createdAt.plusMinutes(3),
+                    startTime = createdAt,
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.PENDING,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "4",
+                    authorizationCode = "",
+                    title = "",
+                    description = DescriptionData(
+                        payment = null,
+                        text = null,
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 0,
+                    endTime = DateTime(0),
+                    startTime = DateTime(0),
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.CONFIRMED,
+                    geolocationRequired = true
+                ),
+                AuthorizationItemViewModel(
+                    authorizationID = "111",
+                    authorizationCode = "",
+                    title = "",
+                    description = DescriptionData(
+                        payment = null,
+                        text = null,
+                        html = null,
+                        extra = null
+                    ),
+                    validSeconds = 0,
+                    endTime = DateTime(0),
+                    startTime = DateTime(0),
+                    connectionID = "1",
+                    connectionName = "Demobank3",
+                    connectionLogoUrl = "url",
+                    apiVersion = "2",
+                    status = AuthorizationStatus.CONFIRMED,
+                    geolocationRequired = true
+                )
+            ),
+            newModelsApiVersion = API_V2_VERSION)
     }
 
     @Test
