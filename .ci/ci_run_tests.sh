9e5607464a44c2a303a5e7c0af2cfcc118a1febd
mnewlive <morozovvadim91@gmail.com>
2024-02-12 16:13:44 +0200

    Replaced license

--- .ci/ci_run_tests.sh
+++ .ci/ci_run_tests.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
-#  This file is part of the Salt Edge Authenticator distribution
-#  (https:github.com/saltedge/sca-authenticator-android)
 #  Copyright (c) 2019 Salt Edge Inc.
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, version 3.
-#
-#  This program is distributed in the hope that it will be useful, but
-#  WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#  General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program. If not, see <http:www.gnu.org/licenses/>.
-#
-#  For the additional permissions granted for Salt Edge Authenticator
-#  under Section 7 of the GNU General Public License see THIRD_PARTY_NOTICES.md
 
 core_report_file="authenticator_core/build/reports/jacocoTestReport/html/index.html"
 sdk_v1_report_file="authenticator_sdk_v1/build/reports/jacocoTestReport/html/index.html"
